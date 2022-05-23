7adf165189b70e3a88e133c2a800bd6525e6aa58
Isaac Powell <powelli91@outlook.com>
2022-05-23 16:41:03 -0700

    working on simple piece to read in user move input

--- hello.rs
+++ hello.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello World!");
+    let mut line = String::new();
+    println!("Enter your move: ");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Move was: {}", line);
+    println!("no of bytes read , {}", b1);
 }
