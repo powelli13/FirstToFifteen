1c825ef4ba6c32c33b3d4681b37dc66f845c34c5
Isaac Powell <powelli91@outlook.com>
2022-05-24 20:24:14 -0700

    just tinkering to keep up the daily commits. i am tired.

--- hello.rs
+++ hello.rs
@@ -1,4 +1,12 @@
+// what is the Rust casing convention?
+fn display_board() {
+    println!("Available numbers are: ");
+    // TODO make this an array and comma separate it
+    println!("1, 2, 3, 4, 5, 6, 7, 8, 9");
+}
+
 fn main() {
+    display_board();
     let mut line = String::new();
     println!("Enter your move: ");
     let b1 = std::io::stdin().read_line(&mut line).unwrap();
