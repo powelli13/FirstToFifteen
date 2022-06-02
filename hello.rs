0bc97fad47038db5e58593f59be9babc2d5deef0
Isaac Powell <powelli91@outlook.com>
2022-06-01 20:57:55 -0700

    now using an actual array for the board

--- hello.rs
+++ hello.rs
@@ -1,14 +1,19 @@
-// what is the Rust casing convention?
-fn display_board() {
+fn display_board(board: [i32; 9]) {
     println!("Available numbers are: ");
-    // TODO make this an array and comma separate it
-    println!("1, 2, 3, 4, 5, 6, 7, 8, 9");
+
+    for item in board.iter() {
+        let x: &i32 = item;
+        println!("{}", x);
+    }
 }
 
 fn main() {
-    display_board();
+    let mut board: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    display_board(board);
+
     let mut line = String::new();
     println!("Enter your move: ");
+
     let b1 = std::io::stdin().read_line(&mut line).unwrap();
     println!("Move was: {}", line);
     println!("no of bytes read , {}", b1);
